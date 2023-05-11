a2f7b43e6487658ad6780fb79b4818361e81af67
ravi8555 <ravi8555@gmail.com>
2023-05-11 22:44:07 +0530

    crio

--- Js-web-projects/src/assets/js/crio.js
+++ Js-web-projects/src/assets/js/crio.js
@@ -1,71 +1,168 @@
-var btn = document.getElementById('btn');
-let userInput = document.getElementById('userinput');
-let dispLabel = document.getElementById('label');
-// let userVal = userInput.value
+// https://www.w3resource.com/javascript-exercises/javascript-string-exercises.php
+// https://github.com/lydiahallie/javascript-questions
+// github ref
+// function satHi() {
+//   console.log(name)
+//   console.log(age)
+//   var name = 'Advait'
+//   let age = 7
+// }
 
-// console.log(yyuserVal);
-btn.addEventListener('click', (e)=>{
-    e.preventDefault();
-    let userVal = userInput.value
-    dispLabel.innerHTML = reversNum(userVal)
-       
-})
-function reversNum(n){
-    const  reversed = 0;
-    while(n != 0){
-        lastDigit = n%10;
-        reversed = reversed * 10 + lastDigit;
-        n = Math.floor(n/10);
-    }
-    return reversed
+// satHi()
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log('test0=>', i), 1)
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   setTimeout(() => console.log('test=>', i), 1)
+// }
+
+const sahpe = {
+  radius: 10,
+  diameter() {
+    return this.radius * 2
+  },
+  perameter: () => 2 * Math.PI * this.radius,
 }
 
-var teamLead;
-console.log(teamLead);
+console.log(sahpe.diameter())
+console.log(sahpe.perameter())
 
-var x = 0;
+// var btn = document.getElementById('btn')
+// let userInput = document.getElementById('userinput')
+// let dispLabel = document.getElementById('label')
+// // let userVal = userInput.value
 
-console.log(typeof(typeof(x)));
+// // console.log(yyuserVal);
+// btn.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   let userVal = userInput.value
+//   dispLabel.innerHTML = reversNum(userVal)
+// })
+// function reversNum(n) {
+//   const reversed = 0
+//   while (n != 0) {
+//     lastDigit = n % 10
+//     reversed = reversed * 10 + lastDigit
+//     n = Math.floor(n / 10)
+//   }
+//   return reversed
+// }
 
-//  Seesion -2
+// var teamLead
+// console.log(teamLead)
 
-console.log(10 + 2 * 3);
-console.log((10 + 2) * 3);
+// var x = 0
 
-console.log(10 % 2); /*Reminder0 */
-console.log(10 % 3); 
-console.log(10 % 6); 
+// console.log(typeof typeof x)
 
-console.log("Java"+ "Script"); // Concatination
+// //  Seesion -2
 
-console.log("Javascript" + 1 + 3);
-console.log("Javascript" + 1 + 3 + 5);
+// console.log(10 + 2 * 3)
+// console.log((10 + 2) * 3)
 
-var x = "3";
-var y = true;
-var z = x + y;
-console.log(z);
+// console.log(10 % 2) /*Reminder0 */
+// console.log(10 % 3)
+// console.log(10 % 6)
 
-console.log('java1' * 1 +5 / 3);
+// console.log('Java' + 'Script') // Concatination
 
+// console.log('Javascript' + 1 + 3)
+// console.log('Javascript' + 1 + 3 + 5)
 
-console.log(40>=20);
-console.log(4>=20);
+// var x = '3'
+// var y = true
+// var z = x + y
+// console.log(z)
 
-console.log("3" == 3);
-console.log("3" === 3);
+// console.log('java1' * 1 + 5 / 3)
 
-console.log(true===1);
-console.log(true!==1);
+// console.log(40 >= 20)
+// console.log(4 >= 20)
 
-let a =10;
-let b = 20;
+// console.log('3' == 3)
+// console.log('3' === 3)
 
-console.log(a === b);
-console.log(a >= b);
-console.log(a<=b);
-console.log(a !== b);
+// console.log(true === 1)
+// console.log(true !== 1)
 
-console.log( a !== b && a > b);
-console.log( a !== b || a > b);
+// let a = 10
+// let b = 20
 
+// console.log(a === b)
+// console.log(a >= b)
+// console.log(a <= b)
+// console.log(a !== b)
+
+// console.log(a !== b && a > b)
+// console.log(a !== b || a > b)
+// console.log('test')
+function gradeMarks(marks) {
+  if (marks < 0 || marks > 100) {
+    return 'E'
+  } else if (marks < 25) {
+    return 'E'
+  } else if (marks >= 25 && marks < 45) {
+    return 'F'
+  } else if (marks >= 45 && marks < 50) {
+    return 'D'
+  } else if (marks >= 50 && marks < 60) {
+    return 'C'
+  } else if (marks >= 60 && marks < 80) {
+    return 'B'
+  } else if (marks >= 80 && marks <= 100) {
+    return 'A'
+  } else {
+    return 'Invalid'
+  }
+}
+
+console.log(gradeMarks(0))
+console.log(gradeMarks(101))
+console.log(gradeMarks(-5))
+console.log(gradeMarks(24))
+console.log(gradeMarks(45))
+console.log(gradeMarks(50))
+console.log(gradeMarks(60))
+console.log(gradeMarks(80))
+
+// function checkString(checkString) {
+//   if (typeof checkString === 'string') {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// console.log(checkString([1, 2, 3]));
+
+// function checkBlankString(checkBlankString) {
+//   if (checkBlankString.length === 0) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// console.log(checkBlankString(''))
+
+// function convArray(stringtoarray) {
+//   let conStringArray = stringtoarray.trim().split(' ')
+//   return conStringArray
+// }
+// console.log(convArray('Hi I am Javascript Learner'));
+
+// function exctractChar(charargument, indxStart, idxEnd) {
+//   let getChar = charargument.substring(indxStart, idxEnd).trim()
+//   return getChar
+// }
+// console.log(exctractChar('I Love Javascript', 1, 4));
+
+const getStringChar = (str, length) => {
+  if (str.constuctor == String && length > 0);
+  return str.slice(0, length)
+
+  // return str.slice(0, length)
+}
+console.log(getStringChar('I Love Javascript', 34))
