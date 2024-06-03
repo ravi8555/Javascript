b6daa55d91598e5dff9678596e15606603090d49
lasdgitlab <ravi8555@gmail.com>
2024-06-03 10:14:35 +0530

    modified:   Js-web-projects/src/assets/js/app.js         modified:   Js-web-projects/src/assets/js/revision.js         modified:   Js-web-projects/webpack.config.js

--- Js-web-projects/src/assets/js/revision.js
+++ Js-web-projects/src/assets/js/revision.js
@@ -737,15 +737,38 @@ const imageGallery = [
 //   small: true,
 // };
 
-const bird = {
-  size: 'small',
-};
+// const bird = {
+//   size: 'small',
+// };
 
-const mouse = {
-  name: 'Mickey',
-  small: true,
-};
+// const mouse = {
+//   name: 'Mickey',
+//   small: true,
+// };
 
-// console.log(mouse.bird.size);
-// console.log(mouse[bird.size]);
-console.log(mouse[bird["size"]]);
+// // console.log(mouse.bird.size);
+// // console.log(mouse[bird.size]);
+// console.log(mouse[bird["size"]]);
+
+// let a = 3;
+// let b = new Number(3);
+// let c = 3;
+
+// // console.log(a == b);
+// console.log(a === b);
+// console.log(b === c);
+
+
+class Chameleon {
+  static colorChange(newColor) {
+    this.newColor = newColor;
+    return this.newColor;
+  }
+
+  constructor({ newColor = 'green' } = {}) {
+    this.newColor = newColor;
+  }
+}
+
+const freddie = new Chameleon({ newColor: 'purple' });
+console.log(freddie.newColor);
