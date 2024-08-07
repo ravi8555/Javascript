7c93a79a77792b14b040477317b5fea88288a802
lasdgitlab <ravi8555@gmail.com>
2024-08-07 19:12:57 +0530

    updated

--- Js-web-projects/src/assets/js/DSA/matrix.js
+++ Js-web-projects/src/assets/js/DSA/matrix.js
@@ -1,3 +1,132 @@
+const printMtrix = [
+  [10, 20, 30, 40, 21,],
+  [11, 22, 33, 44, 32,],
+  [50, 60, 70, 80, 41,],
+  [55, 66, 77, 88, 51],
+  [55, 66, 77, 88, 61],
+  
+];
+
+function learnPrintMatrix(mtx){
+  let n = mtx.length;
+  // const findx = mtx 
+  const fColFindx = mtx[0][0] //10
+  const sColTindex = mtx[1][2] // 33
+
+  // corners
+  const topLeft = mtx[0][0] // 10
+  const topRight = mtx[0][n-1] // 40
+  const bottomLeft = mtx[n-1][0] // 55
+  const bottomRight = mtx[n-1][n-1] // 88
+
+  // Rows
+  const firstRow = []
+  const firstRow1=[...mtx][0]
+  const firstRow2=[]
+  const secRow = []
+  const ThiRow = []
+  const FourthRow = []
+  const FourthRow1 = []
+
+  // Coulmns
+  const col1 = []
+  const col2 = []
+  const col3 = []
+  const col4 = []
+  const col41 = []
+
+  // Diagonal
+  let pMidD = [];
+  let sMidD = [];
+
+  let sumP = 0
+  let sumS = 0
+
+
+  for(let i =0 ; i < n; i++){
+
+    // first Row
+    firstRow.push(mtx[0][i]) 
+    firstRow2.push(mtx[n-4][i]) 
+    // // Second Row
+    // secRow.push(mtx[1][i]);
+    // // Third Row
+    // ThiRow.push(mtx[2][i])
+    // // FourthRow
+    FourthRow.push(mtx[3][i]);
+    FourthRow1.push(mtx[n-1][i]);
+
+    // firstCol
+    // col1.push(mtx[i][0])
+    // // secondCol
+    // col2.push(mtx[i][1])    
+    // column 4
+    // col4.push(mtx[i][n-1])
+    // col41.push(mtx[i][3])
+
+
+
+
+    /* these are the elements where the row index and column index are the same.
+    // So, when i is 0, mtx[i][i] is mtx[0][0], which is the first element of the primary diagonal. When i is 1, mtx[i][i] is mtx[1][1], and so on. This way, you traverse the primary diagonal of the matrix. */
+    // pMidD.push(mtx[i][i])
+
+    // secondery Diaogonal
+    // sMidD.push(mtx[i][n-i-1])
+    
+    // add sum primery Diaogonal
+    sumP += mtx[i][i];
+
+    // add sum secondary Diaogonal
+    sumS+=mtx[i][n-i-1]
+
+    // Skiping the middle same number to add in secondary Diaogonal
+    // not equal to even matrix 5x5 
+    if(i !== Math.floor(n/2) ){
+      sumS += mtx[i][n-i-1]
+      sMidD.push(mtx[i][n-i-1])
+    }
+    // return
+
+  } 
+  return [
+    // fColFindx,
+    // sColTindex,
+
+    /* // Corners
+    topLeft,
+    topRight,
+    bottomRight,
+    bottomLeft,*/
+
+    /* Rows*/
+    // firstRow,
+    // firstRow1,
+    // firstRow2,
+    // secRow,
+    // ThiRow,
+    // FourthRow,
+    
+
+    // Coulmns 
+    // col1,
+    // col2,
+    // col4,
+    // col41,
+
+    // pMidD,
+    // sMidD,
+    // totalSum,
+    // sumP,
+    // sumS,
+    sMidD,
+
+    sumP + sumS
+    // last
+  ]
+}
+
+// console.log(learnPrintMatrix(printMtrix));
 // print matrix 
 // function printMatrix(size) {
 //     for (let i = 0; i < size; i++) {
@@ -138,103 +267,45 @@
 
 // console.log("Question => PascalsTriangle Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.Pascal’s triangle is a pattern in which a number in a row b, is the sum of the number exactly above it a, and the number to the left of a, c.");
 
-// function pascalsTriangle(n) {
-//   // make a 2dList of intinger pascal array
-//   const pascal = []
-// // running loop for every row till n-1
-//   for(let row =1; row <n ; row++){
-//     // get the previous list of intinger from pascal 
-//     const prev = (row === 1 ? [] : pascal[pascal.length -1])
+function pascalsTriangle(n) {
+  // make a 2dList of intinger pascal array
+  const pascal = []
+// running loop for every row till n-1
+  for(let row =1; row <=n ; row++){
+    // get the previous list of intinger from pascal 
+    const prev = (row === 1) ? [] : pascal[pascal.length -1]
 
-//     // make 1D list of initigers current
-//     const curr = []
+    // make 1D list of initigers current
+    const curr = []
 
-//     // run the for loop for every coulmns [0-row-1]
-//     for(col = 0 ; col <=row-1; col ++){
-//       // add value as a pascal previous row's col's index
-//       const x= col>= pascal.length ? 0 :prev[col];
+    // run the for loop for every coulmns [0-row-1]
+    for(let col = 0 ; col <=row-1; col ++){
+      // add value as a pascal previous row's col's index
+      const x= col>= pascal.length ? 0 :prev[col];
 
-//       // and add col-1 nth index in the previous array
-//       const y = col-1<0?0 : prev[col-1]
+      // and add col-1 nth index in the previous array
+      const y = col-1<0?0 : prev[col-1]
 
-//       if(row ===1){
-//         curr.push(1)
-//       }
-//       else{
-//         curr.push(x+y)
-//       }
+      if(row === 1){
+        curr.push(1)
+      }
+      else{
+        curr.push(x+y)
+      }
 
-//     }
-//     // add the 1st list current in 2-d list pascal
-//     pascal.push(curr)
+    }
+    // add the 1st list current in 2-d list pascal
+    pascal.push(curr)
 
-//   }
-//   return pascal
-// }
-// // console.log(pascalsTriangle(6));
-
-
-function printNum(matrix){
-  const n = matrix.length;
-  // const fColFRow = matrix[0][0]
-  const fColFRow3 = matrix[n-1][n-1]
-  const fColFRow4 = matrix[3][3]
-  const flatMatrix = matrix.flat();
-  const fColFRow6 = flatMatrix[flatMatrix.length - 1];
-  const fColFRow5 = matrix[n-1].pop() // Access the element directly or keep it below because
-  
-  // const fColFRow1 = matrix[n-1][0]
-  // const fColFRow2 = matrix[n-2][n-3]
-  // const fColTRow = matrix[0][2]
-  // const fColTRow1 = matrix[0][n-2]
-  // const TcolSRow = matrix[2][1]
-  // const lastCol = matrix[n-1][n-1]
-
-  // let fRow = []
-  // let fCol = []
-
-  // for(let i =0; i <n; i++){
-     
-  //   fRow += matrix[i]
-  //   fCol += matrix[i][n-2]
-  // }
-  // return {fRow, fCol }
-  return [
-    fColFRow3,
-    fColFRow4,
-    fColFRow5,
-    fColFRow6,
-    // lastCol
-    // fColFRow,
-    // fColFRow1, 
-    // fColFRow2,
-    // fColFRow
-    // fColTRow, 
-    // fColTRow1, 
-    // TcolSRow, 
-    // TcolSRow1
-  ]
+  }
+  return pascal
 }
-const printMtrix = [
-  [10, 20, 30, 40],
-  [11, 22, 33, 44],
-  [50, 60, 70, 80],
-  [55, 66, 77, 88]
-];
-console.log("printNum",printNum(printMtrix) );
+console.log(pascalsTriangle(5));
 
-function printDiagonal(mtx){
-  let n = mtx.length;
-  const primeD = mtx[0][0]
-  const lastD = mtx[n-1][n-1]
-  let midD = []
-  // for(let i =0 ; i < n; i++){
-  //   midD.push(mtx[i][i])
-  // } 
-  return [lastD]
-}
 
-console.log(printDiagonal(printMtrix));
+
+
+
 
 
 
