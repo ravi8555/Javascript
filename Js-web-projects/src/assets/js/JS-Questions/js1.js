397760a271e7f2739905d7878f2ce90a6df5c177
lasdgitlab <ravi8555@gmail.com>
2025-01-07 18:34:17 +0530

    update

--- Js-web-projects/src/assets/js/JS-Questions/js1.js
+++ Js-web-projects/src/assets/js/JS-Questions/js1.js
@@ -3,12 +3,48 @@ console.log("Question ===>  Check for occurence of substring");
 Find the index of the first occurrence of that substring in the actual string
 Find the index of the last occurrence of that substring in the actual string.
 If both these indexes are the same, then return true. Otherwise, return false. */
-function  checkForSubstr(str, k){
-    if(str.indexOf(k) === str.lastIndexOf(k)){       
-        return true
-    }else{
-        return false
-    }
+// function  checkForSubstr(str, k){
+//     if(str.indexOf(k) === str.lastIndexOf(k)){       
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(checkForSubstr("Next time there won’t be a next time", "time"));
+console.log("Question ===> Write a function that takes a string as argument. The string contains the substring 'is'. Return the index of 'is'.")
+// function getIndexOfsub(str, k){
+//     for(let i = 0; i < str.length - k.length; i++){
+//         if(str.substring(i,  i + k.length) === k){
+//             return  i
+//         }
+//     }
+//     return -1
+// }
+// console.log(getIndexOfsub("function that takes a string as argument", "as"));
+
+
+// function myFunction(a){
+//     const len = a.length;
+//     const firstHalf = a.substring(0,Math.floor(len / 2))
+//     return firstHalf
+// }
+
+// console.log( myFunction("My name is Aman"));
+console.log("Create the function which accepts an array of numbers as input.");
+function checkPositive(arr){
+    // // Approach -1
+    // for(let i =0; i < arr.length; i++){
+    //     if(arr[i] < 0){
+    //         return false
+    //     }
+    // }
+    // return true
+    // // Approach -2
+    return arr.every( function(num){
+     return num >= 0;
+    })
+    // same as=> return arr.every( (num) => num >= 0)
 }
-console.log(checkForSubstr("Next time there won’t be a next time", "time"));
+console.log("checkPositive", checkPositive([1,2,3,4]));
+
 
