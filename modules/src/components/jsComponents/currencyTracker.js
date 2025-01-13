31b76c6126f0f411dad246e21f2097258c41c2d3
ravi8555 <ravi8555@gmail.com>
2025-01-13 23:17:54 +0530

    currencyUpdate

--- modules/src/components/jsComponents/currencyTracker.js
+++ modules/src/components/jsComponents/currencyTracker.js
@@ -1,22 +1,31 @@
 console.log("Currency Tracker Loaded");
-
-async function getCurrency(){
-    const fetchData = async ()=>{
+async function getCurrency(){   
         const respose = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
-        const data = await respose.json();
-        console.log("Data",data);
+        const data = await respose.json();        
         return data
-    }
-    return fetchData()
 }
-getCurrency()
+const disRate = async ()=>{
+    const getrates = await getCurrency();
+    console.log(getrates.rates);
+    return getrates.rates
 
-// async function getCurrency(){   
-//         const respose = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
-//         const data = await respose.json();
-//         console.log("Data",data);
-//         return data
+}
+const handleClick = async (event)=>{
+    event.preventDefault()
+    const result = document.getElementById("results")
+    const input = document.getElementById("currencyInput");
+    const inputVal = input.value.toUpperCase();
+    const rates = await disRate()
 
+    if (rates[inputVal]) { 
+        result.innerHTML = `Rate for ${inputVal}: ${rates[inputVal]}`; 
+    } else { 
+        result.innerHTML = `No rate found for ${inputVal}`;
+    }
+    
+}
+window.getCurrency = getCurrency;
+window.handleClick = handleClick;
+window.disRate = disRate;
+export {getCurrency, handleClick, disRate}
 
-// }
-// getCurrency()
